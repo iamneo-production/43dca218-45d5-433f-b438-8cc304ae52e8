c44d099151881d227924d7a991c1ccfcb43e64a7
KiranKumarSingh18 <kirankumarsingh789@gmail.com>
2022-09-27 10:55:36 +0000

    login sample2

---
+++ Reactapp/src/components/Credential.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Credential() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- Reactapp/src/components/auth/Login.jsx
+++ Reactapp/src/components/auth/Login.jsx
@@ -6,77 +6,79 @@ const Loginpage = () => {
 
     return (
         <>
-        <div
-      class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
-    >
-
-            <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-
-            <form>
-                <div class="flex flex-row items-center justify-center lg:justify-start">
-
-
-
-        {/* <!-- Email input --> */}
-                <div class="mb-6 ">
-                        <input
-                        type="text"
-                        class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        id="exampleFormControlInput2"
-                        placeholder="Email address"
-                        />
-                </div>
-
-
-          {/* <!-- Password input --> */}
-                <div class="mb-6">
-                    <input
-                    type="password"
-                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    id="exampleFormControlInput2"
-                    placeholder="Password"
-                    />
-                </div>
-
-                <div class="flex justify-between items-center mb-6">
-                    <div class="form-group form-check">
-                    <input
-                        type="checkbox"
-                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        id="exampleCheck2"
-                    />
-                    <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
-                        >Remember me</label
-                    >
+<div className="container-fluid bg-gray-300 h-screen flex justify-center">
+            <div class="w-full max-w-xs mt-16  mb-24">
+                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            Enter Email
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" placeholder="Email" />
                     </div>
-                    <a href="#!" class="text-gray-800">Forgot password?</a>
-                </div>
+                    {/* <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            Enter Username
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" type="text" placeholder="Username" />
+                    </div> */}
+                    {/* <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="mobile">
+                            Enter Mobile Number
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="mobile" type="text" placeholder="xxx-xxx-xxxx" />
+                    </div> */}
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            Enter Password
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="*******" />
+                    </div>
+                    {/* <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="cpassword">
+                            Enter Confirm Password
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="cpassword" type="password" placeholder="******" />
+                    </div> */}
+                    {/* <div class="flex items-center justify-between">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
+                        </button>
+                        <a class="flex items-right  inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                            Forgot password?
+                        </a>
+                    </div> */}
 
-          <div class="text-center lg:text-left">
-            <button
-              type="button"
-              class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Login
-            </button>
-            <p class="text-sm font-semibold mt-2 pt-1 mb-0">
-              Don't have an account?
-              <a
-                href="#!"
-                class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                >Register</a
+                    <div class="flex justify-between items-center mb-6">
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                id="exampleCheck2"
+              />
+              <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
+                >Remember me</label
               >
-            </p>
-          </div>
-          </div>
-          </form>
-
-
-
+            </div>
+            <a href="#!" class="text-blue-500 hover:text-blue-800">Forgot password?</a>
           </div>
 
 
-          </div>
+
+                    <div class="mb-4">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+
+
+
+
+        
       </>
     );
 }
