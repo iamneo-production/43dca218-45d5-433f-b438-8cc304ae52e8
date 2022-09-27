776e632c4db0e9d7a6305b7b21cd7df113d170b5
RiderPy <utkarshgoelksp123@gmail.com>
2022-09-27 22:38:19 +0530

    adding wise locker

---
+++ src/main/java/wiselocker/codejava/SignupController.java
@@ -0,0 +1,28 @@
+package wiselocker.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignupController {
+    
+	
+	@Autowired
+	private SignupService signupservice; 
+	
+	@RequestMapping(method=RequestMethod.GET, value="/signup")
+	public boolean checkuser(@RequestBody UserModel usermodel)
+	{
+		if(signupservice.saveUser(usermodel))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
