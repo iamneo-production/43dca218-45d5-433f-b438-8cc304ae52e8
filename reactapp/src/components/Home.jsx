747712d0f0e2999ea59044e95817aa28d7b96b59
kambamswarnakanthreddy <kambamswarnakanth22@gmail.com>
2022-09-28 10:54:34 +0000

    Commit 3

--- reactapp/src/components/Home.jsx
+++ reactapp/src/components/Home.jsx
@@ -6,18 +6,18 @@ const Home = () =>  {
       <div className=" flex justify-between mt-16 border-2 rounded-xl shadow-xl w-10/12">
         <div className="w-6/12 h-[472px] ">
           <div className="mt-4 h-[390px] text- w-full overflow-x-auto overflow-x-hidden px-4 py-4">
-            <div margin-top="50px" className="rounded-2xl px-8 shadow-xl mb-4 border rounded-xl bg-red-400 w-full flex justify-between items-center">
-              <div className="font-bold text-lg">
+            <div className="rounded-2xl px-8 shadow-xl mb-4 border rounded-xl bg-red-400 w-full flex justify-between items-center">
+              <div className="font-bold text-lg py-8">
                 <a href="/cre">Credential Locker</a>
                 </div>
             </div>
             <div className="rounded-2xl px-8 shadow-xl mb-4 border rounded-xl bg-red-400 w-full flex justify-between items-center">
-              <div className="font-bold text-lg">
+              <div className="font-bold text-lg py-8">
                 <a href="/BankInfo"> Bank Details Locker</a>
               </div>
             </div>
             <div className="rounded-2xl px-8 shadow-xl mb-4 border rounded-xl bg-red-400 w-full flex justify-between items-center">
-              <div className="font-bold text-lg">
+              <div className="font-bold text-lg py-8">
                 <a href="/MediaLocker">Media Locker</a>
               </div>
             </div>
@@ -25,7 +25,7 @@ const Home = () =>  {
           </div>
         </div>
 
-        <div margin-top="20px"className="w-8/12 h-[300px] bg-[#fffff]">
+        <div margin-top="20px"className="w-12/12 h-[300px] bg-[#fffff]">
             <img className="image" class="m-4" src="https://media.istockphoto.com/photos/cybersecurity-and-secure-nerwork-concept-data-protection-gdrp-glowing-picture-id1197780051?k=20&m=1197780051&s=612x612&w=0&h=hUVHAXTr1RsEq7sWyDkXGPQzhwg3bUcJDgWKh-d0Qp4=" alt="image"/>
         </div>
       </div>
--- reactapp/src/components/auth/Login.jsx
+++ reactapp/src/components/auth/Login.jsx
@@ -15,40 +15,13 @@ const Loginpage = () => {
                         </label>
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" placeholder="Email" />
                     </div>
-                    {/* <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                            Enter Username
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" type="text" placeholder="Username" />
-                    </div> */}
-                    {/* <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="mobile">
-                            Enter Mobile Number
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="mobile" type="text" placeholder="xxx-xxx-xxxx" />
-                    </div> */}
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                             Enter Password
                         </label>
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="*******" />
                     </div>
-                    {/* <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="cpassword">
-                            Enter Confirm Password
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="cpassword" type="password" placeholder="******" />
-                    </div> */}
-                    {/* <div class="flex items-center justify-between">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                            Sign In
-                        </button>
-                        <a class="flex items-right  inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                            Forgot password?
-                        </a>
-                    </div> */}
-
-                    <div class="flex justify-between items-center mb-6">
+                     <div class="flex justify-between items-center mb-6">
             <div class="form-group form-check">
               <input
                 type="checkbox"
@@ -61,10 +34,7 @@ const Loginpage = () => {
             </div>
             <a href="#!" class="text-blue-500 hover:text-blue-800">Forgot password?</a>
           </div>
-
-
-
-                    <div class="mb-4">
+         <div class="mb-4">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Sign In
                         </button>
